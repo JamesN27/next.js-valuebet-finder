ebe207ec8fd2ba42ceb9088722315a3b10ebc3c4
James Henning <jamesniklas@hotmail.de>
2023-06-21 20:36:52 +0200

    Add logo to navbar

--- app/navbar.js
+++ app/navbar.js
@@ -1,8 +1,16 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import vwlogo from '../public/images/vwlogo.png';
+import style from './navbar.module.css';
 
 export default function Navbar() {
   return (
     <nav>
+      <Link href="http://localhost:3000">
+        <div className={style.logoContainer}>
+          <Image src={vwlogo} alt="Logo" width={500} height={500} />
+        </div>
+      </Link>
       <ul>
         <li>
           <Link href="/">Home</Link>

11819d3b8f7e12f06c92e6f635d4ab875d05ae11
James Henning <jamesniklas@hotmail.de>
2023-06-22 15:01:04 +0200

    Set up registrtion form and grouped routing

---
+++ app/(auth)/register/RegisterForm.js
@@ -0,0 +1,18 @@
+'use client';
+
+export default function RegisterForm() {
+  return (
+    <form>
+      {' '}
+      <label>
+        {' '}
+        username: <input />
+      </label>
+      <label>
+        {' '}
+        password: <input type="password" />{' '}
+      </label>
+      <button onClick={() => {}}> sign up </button>
+    </form>
+  );
+}

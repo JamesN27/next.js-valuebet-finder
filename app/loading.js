f2dc1eae6d85de1dca068f6d665aa9fa302c525d
James Henning <jamesniklas@hotmail.de>
2023-06-22 15:04:12 +0200

    Add not found, loading and error pages

---
+++ app/loading.js
@@ -0,0 +1,8 @@
+export default function RootLoading() {
+  return (
+    <div>
+      Loading... Please bear with us to see the worlds best valuebets in a few
+      seconds
+    </div>
+  );
+}
---
+++ app/not-found.js
@@ -0,0 +1,13 @@
+export const rootNotFoundMetadata = {
+  title: 'Not Found',
+  description: "sorry can't find the page you are looking for",
+};
+
+export default function RootNotFound() {
+  return (
+    <div>
+      Sorry this page was not found, make sure you visit a page that exists to
+      find the worlds best value bets
+    </div>
+  );
+}

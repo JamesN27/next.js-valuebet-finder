63e605a330453a89150da13540b49caafb9c0e9b
James Henning <jamesniklas@hotmail.de>
2023-06-28 10:31:25 +0200

    Set up register form page truly

--- app/(auth)/register/RegisterForm.js
+++ app/(auth)/register/RegisterForm.js
@@ -1,18 +1,39 @@
 'use client';
 
+import { useState } from 'react';
+
 export default function RegisterForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <form>
-      {' '}
+    <form onSubmit={(event) => event.preventDefault()}>
       <label>
-        {' '}
-        username: <input />
+        username:{' '}
+        <input
+          value={username}
+          onChange={(event) => setUsername(event.currentTarget.value)}
+        />
       </label>
       <label>
-        {' '}
-        password: <input type="password" />{' '}
+        password:{' '}
+        <input
+          value={password}
+          type="password"
+          onChange={(event) => setPassword(event.currentTarget.value)}
+        />
       </label>
-      <button onClick={() => {}}> sign up </button>
+      <button
+        onClick={async () => {
+          await fetch('/api/register', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+          });
+        }}
+      >
+        {' '}
+        sign up{' '}
+      </button>
     </form>
   );
 }

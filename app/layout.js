50b24eb07c2e1334835f76440fe34d6c78332de1
James Henning <jamesniklas@hotmail.de>
2023-06-29 20:05:05 +0200

    Add background picture

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import style from './background.module.css';
 import Navbar from './navbar';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -12,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} ${style['background-image']}`}>
         <main>
           <Navbar />
           {children}

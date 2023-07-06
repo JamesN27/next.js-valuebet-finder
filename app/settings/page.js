29114a29ee1b22ee101a4ac2b26372b13737b824
James Henning <jamesniklas@hotmail.de>
2023-07-06 11:43:47 +0200

    Switch valuebets from settings page to valuebet page

--- app/settings/page.js
+++ app/settings/page.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import OddsComponent from '../OddsComponent';
 
 export default function Settings() {
   return (
     <div>
       <h1>Settings Page</h1>
       <p>This is the Settings page content.</p>
-      <OddsComponent />
     </div>
   );
 }
--- app/valuebets/page.js
+++ app/valuebets/page.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import OddsComponent from '../OddsComponent';
 import Calculator from './calculator';
 
 export default function ValueBets() {
   return (
     <div>
       <h1>Value Bets Page</h1>
+      <OddsComponent />
       <Calculator />
     </div>
   );

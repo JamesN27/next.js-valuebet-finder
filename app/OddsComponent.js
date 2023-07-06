ff50a5d50006e17ad411f8ac4e670c5e67db50c6
James Henning <jamesniklas@hotmail.de>
2023-07-06 11:20:28 +0200

    Set up API for ATP wimbledon

---
+++ app/OddsComponent.js
@@ -0,0 +1,88 @@
+'use client';
+
+import axios from 'axios';
+import React, { useState } from 'react';
+
+const OddsComponent = () => {
+  const [odds, setOdds] = useState(null);
+
+  const handleButtonClick = async () => {
+    try {
+      // Make the API request to fetch the data
+      const response = await axios.get(
+        'https://api.the-odds-api.com/v4/sports/tennis_atp_wimbledon/odds/',
+        {
+          params: {
+            apiKey: '2f42f78466de4f4fa0a9c6eff5ed85fa',
+            bookmakers:
+              'pinnacle,williamhill,betfair_ex_eu,betfair_sb_uk,betonlineag,sport888,mrgreen,unibet_eu,matchbook,onexbet',
+            markets: 'h2h',
+          },
+        },
+      );
+
+      const data = response.data;
+
+      // Extract the higher odds from other bookmakers compared to Pinnacle odds
+      const extractedOdds = data.flatMap((event) => {
+        const pinnacleBookmaker = event.bookmakers.find(
+          (bookmaker) => bookmaker.key === 'pinnacle',
+        );
+
+        if (
+          !pinnacleBookmaker ||
+          !pinnacleBookmaker.markets ||
+          !pinnacleBookmaker.markets[0]?.outcomes
+        ) {
+          // Skip the event if pinnacleBookmaker or necessary properties are undefined
+          return [];
+        }
+
+        const pinnacleOutcome = pinnacleBookmaker.markets[0].outcomes[0];
+
+        const higherOdds = event.bookmakers
+          .filter((bookmaker) => bookmaker.key !== 'pinnacle')
+          .flatMap((bookmaker) => {
+            if (!bookmaker.markets || !bookmaker.markets[0]?.outcomes) {
+              // Skip the bookmaker if necessary properties are undefined
+              return [];
+            }
+
+            return bookmaker.markets[0].outcomes
+              .filter((outcome) => outcome.price > pinnacleOutcome.price)
+              .map((outcome) => ({
+                event: `${event.home_team} vs ${event.away_team}`,
+                bookmaker: bookmaker.title,
+                outcome: outcome.name,
+                price: outcome.price,
+              }));
+          });
+
+        return higherOdds;
+      });
+
+      // Set the extracted odds in the state
+      setOdds(extractedOdds);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleButtonClick}>Get Odds</button>
+      {odds && (
+        <ul>
+          {odds.map((odd, index) => (
+            <li key={odd.id}>
+              Event: {odd.event}, Bookmaker: {odd.bookmaker}, Outcome:{' '}
+              {odd.outcome}, Price: {odd.price}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default OddsComponent;
--- app/settings/page.js
+++ app/settings/page.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import OddsComponent from '../OddsComponent';
+
 export default function Settings() {
   return (
     <div>
       <h1>Settings Page</h1>
       <p>This is the Settings page content.</p>
+      <OddsComponent />
     </div>
   );
 }

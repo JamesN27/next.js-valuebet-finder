e5f2267ce089c5ab51d11aa72800918338106c12
James Henning <jamesniklas@hotmail.de>
2023-07-12 17:23:05 +0200

    Fix minor css problems

--- app/nbavalue/page.js
+++ app/nbavalue/page.js
@@ -1,8 +1,12 @@
+import style from './nbavalue.module.css';
+
 export default function ValueBets() {
   return (
-    <div>
+    <div className={style.nbavalue}>
       <h1>NBA Player Prop Value</h1>
-      <p>This is the NBA Player Prop Bets page content.</p>
+      <p>
+        This content will follow in October 2023 when the NBA season starts.
+      </p>
     </div>
   );
 }
